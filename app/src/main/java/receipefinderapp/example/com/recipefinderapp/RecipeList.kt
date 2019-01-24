91311de610a2e307f8069dc17d88c4ea5891cdd3
merisahakyan <sahakyan_m@bk.ru>
2019-01-24 17:43:23 +0400

    Recipe finder project

---
+++ app/src/main/java/receipefinderapp/example/com/recipefinderapp/RecipeList.kt
@@ -0,0 +1,12 @@
+package receipefinderapp.example.com.recipefinderapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_list)
+    }
+}
